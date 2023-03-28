9f0fd1e9b366dc8a1c04da6e9d5e632041f4eea7
domenicobacoli <domenico.bacoli89@gmail.com>
2023-03-28 16:12:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppNavbar from "./components/AppNavbar.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+export default {
+  data() {
+    return {
+
+    };
+  },
+
+  components: {
+    AppNavbar,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+  <AppNavbar></AppNavbar>
+
+  <AppMain></AppMain>
+
+  <AppFooter></AppFooter>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+
+}
+</script>
+
+<template>
+    Navbar
+</template>
+
+<style lang="scss" scoped></style>
